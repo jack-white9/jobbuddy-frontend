cccacb5b45b31af6dd10a1ef530026f354a86e06
Jack White <jfw793@gmail.com>
2023-08-22 13:26:18 +1000

    add state to textarea component

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,7 +1,24 @@
 import "./TextArea.css";
+import { useState } from "react";
 
-function TextArea() {
-  return <textarea className="textarea-textarea" />;
+function TextArea(): JSX.Element {
+  const [text, setText] = useState<string>("");
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(text);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        className="textarea-textarea"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default TextArea;
